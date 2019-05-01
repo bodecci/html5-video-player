934507ca3108140606bf2b0f099bfa7f3de8e3e2
Bode Falade <bode.falade@outlook.com>
2019-05-01 16:01:40 -0500

    video player controls work when clicked and changed.

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ function handleProgress() {
 }
 
 function scrub(e) {
-    console.log(e);
+    
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
     
@@ -58,4 +58,9 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) =>  mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
