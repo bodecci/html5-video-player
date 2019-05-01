cda13d130f14b58c83fdb96946f29405b2986e71
Bode Falade <bode.falade@outlook.com>
2019-05-01 12:17:29 -0500

    video is paused and played when you click it.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,17 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
 // build our functions
+function togglePlay() {
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method](); same as below
+    if(video.paused) { //paused is a property that lives on the video
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
 // hook up the event listeners
+video.addEventListener('click', togglePlay);
